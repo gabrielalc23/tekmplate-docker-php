cd04c2c868f23b4bbec22609bbf1a1bc229a0cfe
gabrielalc23 <gabriellopes.campos23@gmail.com>
2025-03-29 14:25:26 -0300

    php

--- src/routes/web.php
+++ src/routes/web.php
@@ -4,3 +4,14 @@ use Fastroute\RouteCollector;
 use Fastroute\Dispatcher;
 use Fastroute\RouteParser;
 
+require '/path/to/vendor/autoload.php';
+
+
+
+$dispatcher = FastRoute\simpleDispatcher(function(FastRoute\ConfigureRoutes $r) {
+    $r->addRoute('GET', '/users', 'get_all_users_handler');
+
+    $r->addRoute('GET', '/user/{id:\d+}', 'get_user_handler');
+    
+    $r->addRoute('GET', '/articles/{id:\d+}[/{title}]', 'get_article_handler');
+});
